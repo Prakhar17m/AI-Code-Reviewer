c2c29e040a0440c037a20494dea907e32efb4cce
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2026-03-31 17:53:54 +0530

    test3

--- AI Code Reviewer/scripts/review.py
+++ AI Code Reviewer/scripts/review.py
@@ -1,41 +1,41 @@
 import subprocess
-# from google import genai
-# import os
-# import smtplib
-# from email.message import EmailMessage
+from google import genai
+import os
+import smtplib
+from email.message import EmailMessage
 
 
 def getDiff():
-   diff = subprocess.run(["git", "show"], text=True)
+   diff = subprocess.check_output(["git", "show"], text=True)
    return diff
 print(getDiff())
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-# def send_email(html_content):
-#    msg = EmailMessage()
-#    msg['Subject'] = 'Code Review Feedback'
-#    msg['From'] = "prakharmehrotra44@gmail.com"
-#    msg['To'] = "prakharmehrotra44@gmail.com"
-#    msg.set_content("Please find the code review feedback below.")
-#    msg.add_alternative(html_content, subtype='html')
+def send_email(html_content):
+   msg = EmailMessage()
+   msg['Subject'] = 'Code Review Feedback'
+   msg['From'] = "prakharmehrotra44@gmail.com"
+   msg['To'] = "prakharmehrotra44@gmail.com"
+   msg.set_content("Please find the code review feedback below.")
+   msg.add_alternative(html_content, subtype='html')
    
-#    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-#       smtp.login("prakharmehrotra44@gmail.com", os.getenv("MAIL_APP_PASSWORD"))
-#       smtp.send_message(msg)
+   with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+      smtp.login("prakharmehrotra44@gmail.com", os.getenv("MAIL_APP_PASSWORD"))
+      smtp.send_message(msg)
       
-#    return "Email sent successfully"
+   return "Email sent successfully"
 
 
-# def main():
-#     diff = getDiff()
-#     prompt = f"Review the following code changes and provide feedback:\n\n Mandatory: provide the output in html that can use to send in mail\n\n{diff}"
-#     response = client.models.generate_content(
-#        model="gemini-3-flash-preview",
-#        contents=prompt
-#     )
-#     html = response.text   
-#     send_email(html) 
+def main():
+    diff = getDiff()
+    prompt = f"Review the following code changes and provide feedback:\n\n Mandatory: provide the output in html that can use to send in mail\n\n{diff}"
+    response = client.models.generate_content(
+       model="gemini-3-flash-preview",
+       contents=prompt
+    )
+    html = response.text   
+    send_email(html) 
     
-# main()
+main()
     
